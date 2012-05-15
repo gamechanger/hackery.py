66feba7f8f244f1ad04be55489349abc2268d1ba
Kiril Savino <aurumaeus@gmail.com>
2012-05-15 19:43:50 -0400

    some notes

--- hackery/hack.py
+++ hackery/hack.py
@@ -5,7 +5,13 @@ from backend import PrintingBackend
 class Hack(object):
     backend = PrintingBackend()
 
-    def shoud_fire(self):
+    def __enter__(self):
+        return self if self._should_fire() else False
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def _should_fire(self):
         return True
 
     def count(self, event):
--- test/hack.py
+++ test/hack.py
@@ -1,4 +1,27 @@
 __author__ = 'Kiril Savino'
 
+from pyvows import Vows, expect
+
 from hackery import Hack
 from hackery import Backend
+
+@Vows.batch
+class HackTest(Vows.Context):
+    def topic(self):
+        return None
+
+    def should_be_none(self, topic):
+        expect(topic).to_be_null()
+
+
+"""
+- Frequency
+- Lifespan
+- Conditions: API Client, Time, Etc.
+- Expiration Dates
+- Alerts: expiration! frequency change!  etc.!
+"""
+
+with Hack('fancy_stuff') as hack:
+    if hack:
+        pass
