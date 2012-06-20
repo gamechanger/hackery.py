fb34646876c1d28a139ce90eaa87af0fda0f00bc
Doug Woos <doug@gamechanger.io>
2012-06-20 02:01:19 -0400

    Hacks have names

--- hackery/backend.py
+++ hackery/backend.py
@@ -5,6 +5,10 @@ from collections import defaultdict
 class Backend(object):
     def __init__(self):
         self.counts = defaultdict(lambda:0)
+        self.hacks = set()
+
+    def call(self, name):
+        self.hacks.add(name)
 
     def count(self, event):
         event = str(event)
@@ -14,7 +18,10 @@ class Backend(object):
         return self.counts.get(event) or 0
 
 class PrintingBackend(Backend):
-    def count(self, event):
-        Backend.count(self, event)
-        print "{0} happened {1} times so far".format(event, self._count_of(event))
+    def call(self, name):
+        super(PrintingBackend, self).call(name)
+        print "{0} called".format(name)
 
+    def count(self, event):
+        super(PrintingBackend, self).count(self, event)
+        print "{0} happened {1} times so far".format(event, self._count_of(event))
--- hackery/hack.py
+++ hackery/hack.py
@@ -4,9 +4,15 @@ from backend import PrintingBackend
 
 class Hack(object):
     backend = PrintingBackend()
+    def __init__(self, name):
+        self.name = name
+        self.backend.call(self.name)
 
     def __enter__(self):
-        return self if self._should_fire() else False
+        if self._should_fire():
+            self.backend.count(self.name)
+            return self
+        return False
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
@@ -15,5 +21,4 @@ class Hack(object):
         return True
 
     def count(self, event):
-        self.backend.count(event)
-
+        self.backend.count('{0}.{1}'.format(self.name, event))
